78c0826d88bd5b1a1d1ee45185972994e46e24b7
pranshugarg <pranshugarg02@gmail.com>
2019-03-15 20:36:52 +0530

    Hashing password before storing in database

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,22 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$huAU4qTnQuGPifHEXfV9cOmPJ7p61oKaoXrY1WviiDAznE/rW8oLK';
+
+bcrypt.compare('123!', hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+
 
 var data = {
   id: 10
